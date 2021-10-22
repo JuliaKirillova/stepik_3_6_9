6cb09f84f9a71eb4114926fa40de79b3d1a2988d
JuliaKirillova <panic_motion@mail.ru>
2021-10-22 19:32:45 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,27 +2,17 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# В коде реализован выбор языка для Chrome, так как это является достаточным для задания (см. п.5)
-# Реализован выбор только для двух языков, согласно условиям задачи
-
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
-                        help="Choose language: 'es' or 'fr'")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose the any language: ")
 
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
-    if user_language == "fr":
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "es":
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
-    else:
-        raise pytest.UsageError("--language should be 'es' or 'fr'")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
-    print("\nquit browser..")
+
     browser.quit()
