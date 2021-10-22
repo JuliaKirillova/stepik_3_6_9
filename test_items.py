a634c5ce033f4fbcdc22876619cecb99cdc1ca23
JuliaKirillova <panic_motion@mail.ru>
2021-10-22 19:10:44 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -13,7 +13,7 @@ def test_find_add_to_basket_button(browser):
     browser.get(link)
     # Расскоментировать time.sleep(30) для проверки задания (см. критерии проверки п.2)
     # time.sleep(30)
-    button = browser.find_elements_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
+    button = browser.find_element_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
     button_text = button.text
     # с помощью assert проверяем наличие кнопки, в случае отсутсвия появится сообщение 'Not Found'
     assert button_text is not None, 'Not Found'
